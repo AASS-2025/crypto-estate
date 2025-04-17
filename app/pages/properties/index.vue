a09802660842dac114958e9632e691a9fe3f4042
Branislav Hozza <brankohozza@gmail.com>
2025-04-17 11:16:00 +0200

    fix: structure

--- app/pages/properties/index.vue
+++ app/pages/properties/index.vue
@@ -1,18 +1,17 @@
 <template>
   <div>
     <ModalsCreateProperty />
-    <ul>
-      <li v-if="status === 'pending'">Loading...</li>
-      <li v-if="status === 'error'">Error loading properties</li>
-      <li v-if="status === 'success' && data">
-        <h2>My Properties</h2>
-        <p>Here are the properties you have listed:</p>
-        <p v-if="data.length === 0">You have not listed any properties yet</p>
-        <div v-for="property in data" :key="property.realEstate.id">
-          <h2>{{ property.realEstate.name }}</h2>
-          <p>{{ property.realEstate.description }}</p>
-          <p>{{ property.realEstate.propertyAddress }}</p>
-        </div>
+    <h2 v-if="status === 'pending'">Loading...</h2>
+    <h2 v-else-if="status === 'error'">Error loading properties</h2>
+    <h2 v-else>My Properties</h2>
+    <p v-if="data && data.length === 0">
+      You have not listed any properties yet
+    </p>
+    <ul v-if="data">
+      <li v-for="property in data" :key="property.realEstate.id">
+        <h2>{{ property.realEstate.name }}</h2>
+        <p>{{ property.realEstate.description }}</p>
+        <p>{{ property.realEstate.propertyAddress }}</p>
       </li>
     </ul>
   </div>
